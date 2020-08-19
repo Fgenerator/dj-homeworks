2bad9d419b05e5b9e9c876ee2cf5284b7a76c348
Evgeny Ermakov <desteev@gmail.com>
2020-08-19 20:08:12 +0500

    Edited: index(), landing(), stats()

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -11,24 +11,45 @@ counter_click = Counter()
 
 
 def index(request):
+    from_landing = request.GET.get('from-landing')
+    if from_landing == 'test':
+        counter_click['test'] += 1
+    if from_landing == 'original':
+        counter_click['original'] += 1
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
     return render_to_response('index.html')
 
 
 def landing(request):
+    ab_test = request.GET.get('ab-test-arg')
+    if ab_test == 'original':
+        counter_show['original'] += 1
+        return render_to_response('landing.html')
+    if ab_test == 'test':
+        counter_show['test'] += 1
+        return render_to_response('landing_alternate.html')
     # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render_to_response('landing.html')
 
 
 def stats(request):
+    print(counter_click)
+    print(counter_show)
+    try:
+        test_conversation = counter_click['test'] / counter_show['test']
+    except ZeroDivisionError:
+        test_conversation = 0
+    try:
+        original_conversation = counter_click['original'] / counter_show['original']
+    except ZeroDivisionError:
+        original_conversation = 0
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Чтобы отличить с какой версии лендинга был переход
     # проверяйте GET параметр marker который может принимать значения test и original
     # Для вывода результат передайте в следующем формате:
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test_conversation,
+        'original_conversion': original_conversation,
     })
