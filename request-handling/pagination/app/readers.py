495496b23db52532823259113a7188d60fa3ef79
Evgeny Ermakov <desteev@gmail.com>
2020-08-20 10:35:08 +0500

    minor edits

--- request-handling/pagination/app/readers.py
+++ request-handling/pagination/app/readers.py
@@ -2,9 +2,7 @@ import csv
 
 
 def read_csv(file):
-    data = []
     with open(file, encoding='windows-1251') as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in reader:
-            data.append(row)
+        data = list(reader)
     return data
