97130480302f020d0f6ffb2497381899fbfae6aa
Evgeny Ermakov <desteev@gmail.com>
2020-08-23 15:17:10 +0500

    added reading data from csv and fill context

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from app.readers import read_csv
+
+DATA = read_csv('inflation_russia.csv')
+
+
 def inflation_view(request):
     template_name = 'inflation.html'
 
     # чтение csv-файла и заполнение контекста
-    context = {}
+    context = {
+        'data': DATA
+    }
 
     return render(request, template_name,
                   context)
