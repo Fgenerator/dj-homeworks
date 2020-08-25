adb6f823b420a103c89260091f62d009275c762f
Evgeny Ermakov <desteev@gmail.com>
2020-08-25 08:38:39 +0500

    Added urls for file_list view and file_content view Added DataConverter registration

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+import datetime
+
+from django.urls import path, register_converter
 
 # Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
+from app import converters
+from app.views import file_list, file_content
 
+register_converter(converters.DateConverter, 'dt')
 
 urlpatterns = [
+    path('', file_list, name='file_list'),
+    path('<dt:date>', file_list, name='file_list'),
+    path('file_content/<name>/', file_content, name='file_content')
     # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
     # path(..., name='file_list'),
     # path(..., name='file_list'),    # задайте необязательный параметр "date"
