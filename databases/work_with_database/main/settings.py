ef3138b9b60965deeb064113dc4d2359b9bc7e37
Evgeny Ermakov <desteev@gmail.com>
2020-08-26 16:49:35 +0500

    Added LOGGING

--- databases/work_with_database/main/settings.py
+++ databases/work_with_database/main/settings.py
@@ -115,6 +115,27 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG', # os.getenv('DJANGO_LOG_LEVEL', 'INFO')
+            'propagate': False,
+        },
+    },
+}
+
 try:
     from .settings_local import *
 except ImportError:
