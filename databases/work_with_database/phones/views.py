bd1067bae2ccb1eaf36798f35be3eeadae6210a6
Evgeny Ermakov <desteev@gmail.com>
2020-08-26 16:49:50 +0500

    Edit show_catalog view

--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from phones.models import Phone
+
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+
+    phones = Phone.objects.all()
+
+    context = {
+        'players': phones,
+    }
     return render(request, template, context)
 
 
