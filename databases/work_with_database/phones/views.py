04013a4cf8912acb4bd4836b51ca38b289d59ab6
Evgeny Ermakov <desteev@gmail.com>
2020-08-27 14:11:11 +0500

    Edited show_catalog, show_product views

--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -6,15 +6,29 @@ from phones.models import Phone
 def show_catalog(request):
     template = 'catalog.html'
 
-    phones = Phone.objects.all()
+    filter = request.GET.get('sort')
+
+    if filter == 'name':
+        phones = Phone.objects.order_by('name')
+    elif filter == 'min_price':
+        phones = Phone.objects.order_by('price')
+    elif filter == 'max_price':
+        phones = Phone.objects.order_by('-price')
+    else:
+        phones = Phone.objects.order_by('name')
 
     context = {
-        'players': phones,
+        'phones': phones,
     }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    phone = Phone.objects.filter(slug=slug).first()
+
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
