878d1af022f79bb4f9e5a14e42608c236edaf6fc
Evgeny Ermakov <desteev@gmail.com>
2020-09-02 13:20:02 +0500

    Added Section, Relationship

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -14,3 +14,35 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Section(models.Model):
+
+    name = models.CharField(max_length=64, verbose_name='Название')
+    articles = models.ManyToManyField(
+        Article,
+        through='Relationship',
+        related_name='sections'
+    )
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class Relationship(models.Model):
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE
+    )
+    section = models.ForeignKey(
+        Section,
+        on_delete=models.CASCADE
+    )
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    def __str__(self):
+        return f'{self.article}: {self.section}'
