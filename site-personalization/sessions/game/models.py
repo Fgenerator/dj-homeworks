f93a25d063ab0ef5e511aec6d0e0a0c00310f145
Evgeny Ermakov <desteev@gmail.com>
2020-09-18 20:54:50 +0500

    Added fields to Game and PlayerGameInfo models

--- site-personalization/sessions/game/models.py
+++ site-personalization/sessions/game/models.py
@@ -6,8 +6,18 @@ class Player(models.Model):
 
 
 class Game(models.Model):
-    pass
+    number = models.IntegerField(default=0)
+    is_finished = models.BooleanField(default=False)
+    final_attempts = models.IntegerField(default=0)
+    players = models.ManyToManyField(
+        Player,
+        through='PlayerGameInfo',
+        default=None,
+    )
 
 
 class PlayerGameInfo(models.Model):
-    pass
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    is_author = models.BooleanField(default=False)
+    attempts = models.IntegerField(default=0)
