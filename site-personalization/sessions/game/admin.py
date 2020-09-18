2026b41dc560f69ff5137d6caa85804bbe97f206
Evgeny Ermakov <desteev@gmail.com>
2020-09-18 20:53:38 +0500

    Added admin for models: Player, Game, PlayerGameInfo

--- site-personalization/sessions/game/admin.py
+++ site-personalization/sessions/game/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+
+from .models import Player, Game, PlayerGameInfo
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'number', 'is_finished', 'final_attempts')
+
+
+class PlayerGameInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'player', 'game', 'is_author', 'attempts')
+
+
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(PlayerGameInfo, PlayerGameInfoAdmin)
