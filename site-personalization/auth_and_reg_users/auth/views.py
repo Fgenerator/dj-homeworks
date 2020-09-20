b1347974a103eb24931be1f9d70b851c0ec763e4
Evgeny Ermakov <desteev@gmail.com>
2020-09-20 15:49:13 +0500

    signup view written

--- site-personalization/auth_and_reg_users/auth/views.py
+++ site-personalization/auth_and_reg_users/auth/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 def home(request):
@@ -9,7 +10,16 @@ def home(request):
 
 
 def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = UserCreationForm()
+
     return render(
         request,
-        'signup.html'
+        'signup.html',
+        {'form': form}
     )
