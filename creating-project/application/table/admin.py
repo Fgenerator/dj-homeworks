1abbcb8d03768a0c3bc57868f9ff03d0f131634a
Evgeny Ermakov <desteev@gmail.com>
2020-09-22 21:02:34 +0500

    Added and registered ColumnAdmin, DataAdmin

--- creating-project/application/table/admin.py
+++ creating-project/application/table/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Column, Data
+
+
+class ColumnAdmin(admin.ModelAdmin):
+    list_display = ('name', 'width', 'serial_number')
+
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ('file_path',)
+
+
+admin.site.register(Column, ColumnAdmin)
+admin.site.register(Data, DataAdmin)
