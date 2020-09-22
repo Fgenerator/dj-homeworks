d0af35b6d3eb0922fc8e1e6235ffc631ae072054
Evgeny Ermakov <desteev@gmail.com>
2020-09-22 21:03:03 +0500

    Added Column model, added Data model with get_path and set_path classmethods

--- creating-project/application/table/models.py
+++ creating-project/application/table/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 
+class Column(models.Model):
+    name = models.CharField(max_length=30)
+    width = models.IntegerField()
+    serial_number = models.IntegerField()
+
+
+class Data(models.Model):
+    file_path = models.FilePathField()
+
+    @classmethod
+    def get_path(cls):
+        path = cls.objects.all()[0]
+        return path
+
+    @classmethod
+    def set_path(cls, path):
+        cls.objects.all().delete()
+        cls.objects.create(file_path=path)
+
