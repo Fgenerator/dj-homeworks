1fe7f86a2663f40a12d6ee59d5754b11c36479e0
Evgeny Ermakov <desteev@gmail.com>
2020-09-23 21:07:08 +0500

    Added Station and Route models

--- creating-project/project/app/models.py
+++ creating-project/project/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    routes = models.ManyToManyField('Route', related_name='stations')
+    name = models.CharField(max_length=100)
+
+class Route(models.Model):
+    name = models.CharField
