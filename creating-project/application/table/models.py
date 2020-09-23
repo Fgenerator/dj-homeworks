60b8c81247b23175b139a77a8a9f9ed8dd580102
Evgeny Ermakov <desteev@gmail.com>
2020-09-23 15:37:41 +0500

    tiny edits

--- creating-project/application/table/models.py
+++ creating-project/application/table/models.py
@@ -4,19 +4,20 @@ from django.db import models
 class Column(models.Model):
     name = models.CharField(max_length=30)
     width = models.IntegerField()
-    serial_number = models.IntegerField()
+    serial_number = models.IntegerField(unique=True)
 
 
 class Data(models.Model):
-    file_path = models.FilePathField()
+    file_path = models.CharField(max_length=100)
 
     @classmethod
     def get_path(cls):
-        path = cls.objects.all()[0]
+        path = cls.objects.all()[0].file_path
         return path
 
     @classmethod
     def set_path(cls, path):
+        from pathlib import Path
         cls.objects.all().delete()
-        cls.objects.create(file_path=path)
+        cls.objects.create(file_path=Path(path))
 
