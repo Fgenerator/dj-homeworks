0c4a66ccb33d58bae9d4908e177436ffe6c719e3
Evgeny Ermakov <desteev@gmail.com>
2020-09-24 16:53:50 +0500

    Added stations/ path with view

--- creating-project/project/project/urls.py
+++ creating-project/project/project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import station_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('stations/', station_view)
 ]
