4b208f3ba530e496b6c03e7fae53fdb37cfa4310
Evgeny Ermakov <desteev@gmail.com>
2020-09-24 16:52:05 +0500

    Added and registered StationAdmin and RouteAdmin

--- creating-project/project/app/admin.py
+++ creating-project/project/app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Station, Route
+
+
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'latitude', 'longitude', 'name', 'get_routes')
+
+
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+admin.site.register(Station, StationAdmin)
+admin.site.register(Route, RouteAdmin)
