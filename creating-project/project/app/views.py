6c8069b4cd316dab05d97d6fe1a20f2e7dad2b9a
Evgeny Ermakov <desteev@gmail.com>
2020-09-24 16:54:08 +0500

    Added station_view and get_center function

--- creating-project/project/app/views.py
+++ creating-project/project/app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Station, Route
+
+
+def get_center():
+    max_longitude = Station.objects.all().order_by('-longitude').first().longitude
+    min_longitude = Station.objects.all().order_by('longitude').first().longitude
+
+    max_latitude = Station.objects.all().order_by('-latitude').first().latitude
+    min_latitude = Station.objects.all().order_by('latitude').first().latitude
+
+    center_longitude = (max_longitude + min_longitude) / 2
+    center_latitude = (max_latitude + min_latitude) / 2
+
+    center = {
+        'y': round(center_latitude, 2),
+        'x': round(center_longitude, 2)
+    }
+    return center
+
+
+def station_view(request):
+    template = 'stations.html'
+
+    route = request.GET.get('route')
+
+    stations = Station.objects.all()
+    routes = Route.objects.all()
+
+    context = {
+        'stations': stations,
+        'routes': routes,
+        'route': route,
+        'center': get_center()
+    }
+
+    return render(request, template, context)
