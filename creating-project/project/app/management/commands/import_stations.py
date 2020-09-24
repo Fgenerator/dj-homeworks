1ed61e8c512f10cda191f0133ff89983ca003578
Evgeny Ermakov <desteev@gmail.com>
2020-09-24 16:52:21 +0500

    import stations from csv to db

--- creating-project/project/app/management/commands/import_stations.py
+++ creating-project/project/app/management/commands/import_stations.py
@@ -17,3 +17,12 @@ class Command(BaseCommand):
             for row in csv_reader:
                 print(row['Latitude_WGS84'], row['Longitude_WGS84'], row['Name'])
                 print(row['RouteNumbers'].split(';'))
+                station = Station(
+                    latitude=row['Latitude_WGS84'],
+                    longitude=row['Longitude_WGS84'],
+                    name=row['Name'],
+                )
+                station.save()
+                for route_name in row['RouteNumbers'].split(';'):
+                    route, created = Route.objects.get_or_create(name=route_name)
+                    station.routes.add(route)
