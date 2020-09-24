d7e54f02eb69aa179807f8e4b6629cdf8309ef92
Evgeny Ermakov <desteev@gmail.com>
2020-09-24 16:53:02 +0500

    Added get_routes for StationAdmin and __str__ to Route

--- creating-project/project/app/models.py
+++ creating-project/project/app/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 
 class Station(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    routes = models.ManyToManyField('Route', related_name='stations')
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+    routes = models.ManyToManyField(
+        'Route',
+        related_name='stations',
+        default=None
+    )
     name = models.CharField(max_length=100)
 
+    def get_routes(self):
+        return " ".join([route.name for route in self.routes.all()])
+
+
 class Route(models.Model):
-    name = models.CharField
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
